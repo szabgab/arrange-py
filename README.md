cf1c5975124daa54d7d9f3511afcd2db47718ec1
Gabor Szabo <gabor@szabgab.com>
2020-03-31 10:08:49 +0300

    import from askubuntu.com

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 Originals:
 https://askubuntu.com/questions/631392/saving-and-restoring-window-positions/631467#631467
+https://askubuntu.com/questions/193569/is-there-a-way-to-store-the-current-desktop-layout
--- arrange.py
+++ arrange.py
@@ -2,6 +2,7 @@
 import subprocess
 import os
 import sys
+import time
 
 wfile = os.environ["HOME"]+"/.windowlist"
 arg = sys.argv[1]
@@ -16,27 +17,72 @@ def check_window(w_id):
     else:
         return False
 
-def read_windows():
-    w_list =  [l.split()[:6] for l in get("wmctrl -lG").splitlines()]
-    relevant = [(" ").join(w) for w in w_list if check_window(w[0]) == True]
-    with open(wfile, "wt") as out:
-        for item in relevant:
-            out.write(item+"\n")
+def get_res():
+    # get resolution and the workspace correction (vector)
+    xr = subprocess.check_output(["xrandr"]).decode("utf-8").split()
+    pos = xr.index("current")
+    res = [int(xr[pos+1]), int(xr[pos+3].replace(",", "") )]
+    vp_data = subprocess.check_output(["wmctrl", "-d"]).decode("utf-8").split()
+    curr_vpdata = [int(n) for n in vp_data[5].split(",")]
+    return [res, curr_vpdata]
 
-def restore_windows():
+app = lambda pid: subprocess.check_output(["ps", "-p",  pid, "-o", "comm="]).decode("utf-8").strip()
+
+def read_windows():
+    res = get_res()
+    w_list =  [l.split() for l in get("wmctrl -lpG").splitlines()]
+    relevant = [[w[2],[int(n) for n in w[3:7]]] for w in w_list if check_window(w[0]) == True]
+    for i, r in enumerate(relevant):      
+        relevant[i] = app(r[0])+" "+str((" ").join([str(n) for n in r[1]]))
+    with open(wfile, "wt") as out:
+        for l in relevant:
+            out.write(l+"\n")
+
+def open_appwindow(app, x, y, w, h):
+    ws1 = get("wmctrl -lp"); t = 0
+    # fix command for certain apps that open in new tab by default
+    if app == "gedit":
+        option = " --new-window"
+    else:
+        option = ""
+    # fix command if process name and command to run are different
+    if "gnome-terminal" in app:
+        app = "gnome-terminal"
+    elif "chrome" in app:
+        app = "/usr/bin/google-chrome-stable"
+
+
+    subprocess.Popen(["/bin/bash", "-c", app+option])
+    # fix exception for Chrome (command = google-chrome-stable, but processname = chrome)
+    app = "chrome" if "chrome" in app else app
+    while t < 30:      
+        ws2 = [w.split()[0:3] for w in get("wmctrl -lp").splitlines() if not w in ws1]
+        procs = [[(p, w[0]) for p in get("ps -e ww").splitlines() \
+                  if app in p and w[2] in p] for w in ws2]
+        if len(procs) > 0:
+            time.sleep(0.5)
+            w_id = procs[0][0][1]
+            cmd1 = "wmctrl -ir "+w_id+" -b remove,maximized_horz"
+            cmd2 = "wmctrl -ir "+w_id+" -b remove,maximized_vert"
+            cmd3 = "wmctrl -ir "+procs[0][0][1]+" -e 0,"+x+","+y+","+w+","+h
+            for cmd in [cmd1, cmd2, cmd3]:   
+                subprocess.call(["/bin/bash", "-c", cmd])
+            break
+        time.sleep(0.5)
+        t = t+1
+
+def run_remembered():
+    res = get_res()[1]
     try:
-        wlist = [l.split() for l in open(wfile).read().splitlines()]
+        lines = [l.split() for l in open(wfile).read().splitlines()]
+        for l in lines:          
+            l[1] = str(int(l[1]) - res[0]); l[2] = str(int(l[2]) - res[1] - 24)
+            open_appwindow(l[0], l[1], l[2], l[3], l[4])   
     except FileNotFoundError:
         pass
-    else:
-        for w in wlist:
-            try:
-                cmd = "wmctrl -ir "+w[0]+" -e 0,"+(",").join(w[2:])
-                subprocess.Popen(["/bin/bash", "-c", cmd])
-            except:
-                pass
 
-if arg == "-restore":
-    restore_windows()
-elif arg == "-get":
+if arg == "-run":
+    run_remembered()
+elif arg == "-read":
     read_windows()
+
