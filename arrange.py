9c35a05314d731eb089af0c757d288a6c5c70db9
Gabor Szabo <gabor@szabgab.com>
2020-03-31 10:10:34 +0300

    remove trailing spaces

--- arrange.py
+++ arrange.py
@@ -32,7 +32,7 @@ def read_windows():
     res = get_res()
     w_list =  [l.split() for l in get("wmctrl -lpG").splitlines()]
     relevant = [[w[2],[int(n) for n in w[3:7]]] for w in w_list if check_window(w[0]) == True]
-    for i, r in enumerate(relevant):      
+    for i, r in enumerate(relevant):
         relevant[i] = app(r[0])+" "+str((" ").join([str(n) for n in r[1]]))
     with open(wfile, "wt") as out:
         for l in relevant:
@@ -55,7 +55,7 @@ def open_appwindow(app, x, y, w, h):
     subprocess.Popen(["/bin/bash", "-c", app+option])
     # fix exception for Chrome (command = google-chrome-stable, but processname = chrome)
     app = "chrome" if "chrome" in app else app
-    while t < 30:      
+    while t < 30:
         ws2 = [w.split()[0:3] for w in get("wmctrl -lp").splitlines() if not w in ws1]
         procs = [[(p, w[0]) for p in get("ps -e ww").splitlines() \
                   if app in p and w[2] in p] for w in ws2]
@@ -65,7 +65,7 @@ def open_appwindow(app, x, y, w, h):
             cmd1 = "wmctrl -ir "+w_id+" -b remove,maximized_horz"
             cmd2 = "wmctrl -ir "+w_id+" -b remove,maximized_vert"
             cmd3 = "wmctrl -ir "+procs[0][0][1]+" -e 0,"+x+","+y+","+w+","+h
-            for cmd in [cmd1, cmd2, cmd3]:   
+            for cmd in [cmd1, cmd2, cmd3]:
                 subprocess.call(["/bin/bash", "-c", cmd])
             break
         time.sleep(0.5)
@@ -75,9 +75,9 @@ def run_remembered():
     res = get_res()[1]
     try:
         lines = [l.split() for l in open(wfile).read().splitlines()]
-        for l in lines:          
+        for l in lines:
             l[1] = str(int(l[1]) - res[0]); l[2] = str(int(l[2]) - res[1] - 24)
-            open_appwindow(l[0], l[1], l[2], l[3], l[4])   
+            open_appwindow(l[0], l[1], l[2], l[3], l[4])
     except FileNotFoundError:
         pass
 
